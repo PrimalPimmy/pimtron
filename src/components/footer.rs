21aaaae4f391abea5cfd74cfc76497a345ae338a
PrimalPimmy <Prashant20.pm@gmail.com>
2025-12-04 03:35:43 +0530

    Nix updates
    Signed-off-by: PrimalPimmy <Prashant20.pm@gmail.com>

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -5,7 +5,7 @@ stylance::import_style!(style, "../styles/footer.module.css");
 pub fn Footer() -> impl IntoView {
     view! {
         <footer class=style::footer>
-            <p class=style::text>"© 2025 Pimtron. All systems normal."</p>
+            <p class=style::text>"© 2025 Pimtron. Made with Leptos, Deployed with Nix. All systems normal."</p>
         </footer>
     }
 }
