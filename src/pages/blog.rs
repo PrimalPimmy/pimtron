4cc6a3f923272fdd25844536e1e5bd50ba7c9003
PrimalPimmy <Prashant20.pm@gmail.com>
2025-12-04 14:47:16 +0530

    Hover fix
    Signed-off-by: PrimalPimmy <Prashant20.pm@gmail.com>

--- src/pages/blog.rs
+++ src/pages/blog.rs
@@ -1,18 +1,30 @@
 use leptos::prelude::*;
+use leptos_router::hooks::use_navigate;
+use std::time::Duration;
 use crate::content::get_all_posts;
 stylance::import_style!(style, "../styles/blog.module.css");
 
 #[component]
 pub fn Blog() -> impl IntoView {
     let posts = get_all_posts();
+    let navigate = use_navigate();
 
     view! {
         <div class=style::container>
             <h1 class=style::title>"Latest Blog Posts"</h1>
             <div class=style::post_list>
                 {posts.into_iter().map(|post| {
+                    let navigate = navigate.clone();
+                    let dest = format!("/blog/{}", post.slug);
                     view! {
-                        <a href=format!("/blog/{}", post.slug) class=style::article_card>
+                        <a href=dest.clone() class=style::article_card on:click=move |e| {
+                            e.prevent_default();
+                            let navigate = navigate.clone();
+                            let dest = dest.clone();
+                            set_timeout(move || {
+                                navigate(&dest, Default::default());
+                            }, Duration::from_millis(200));
+                        }>
                             <h2 class=style::article_title>{post.title}</h2>
                             <p class=style::date>{post.date.clone()}</p>
                             <p class=style::summary>{post.summary}</p>
