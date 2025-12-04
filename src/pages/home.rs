13e4b1e59fed45fb47c46da2b4237a9c53cb8162
PrimalPimmy <Prashant20.pm@gmail.com>
2025-12-04 14:14:17 +0530

    Hover fix
    Signed-off-by: PrimalPimmy <Prashant20.pm@gmail.com>

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -16,7 +16,6 @@ pub fn Home() -> impl IntoView {
                 </p>
                 <div class=style::actions>
                     <a href="/blog" class=style::btn_primary>"Read my blog"</a>
-                    <a href="https://github.com/primalpimmy" target="_blank" class=style::btn_secondary>"Check GitHub"</a>
                 </div>
             </div>
         </div>
